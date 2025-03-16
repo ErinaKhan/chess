f671fc958d4156a2b1f3f0d2a571e58d785a3ed0
JackMilner1 <117851346+JackMilner1@users.noreply.github.com>
2025-03-16 15:14:16 +0000

    tutorial page selection screen

--- tutorialPage.py
+++ tutorialPage.py
@@ -15,41 +15,77 @@ pygame.display.set_caption('DEMO')
 
 INNER_SCREEN_WIDTH = SCREEN_WIDTH * 0.8
 TUTORIAL_SECTION_WIDTH = INNER_SCREEN_WIDTH * 0.85
-TUTORIAL_SECTION_PADDING = 50
+TUTORIAL_SECTION_PADDING = 80
 
 back_btn = pygame.image.load(r"imagesMisc\undo.png")
+arrowU = pygame.image.load(r"imagesMisc\arrow_up.png")#
+arrowD = pygame.image.load(r"imagesMisc\arrow_down.png")
 exitButton = utils.Button(20,SCREEN_HEIGHT * 0.8,back_btn,0.25)
+arrowButtonUp = utils.Button(SCREEN_WIDTH * 0.85,SCREEN_HEIGHT * 0.08,arrowU,0.35)
+arrowButtonDown = utils.Button(SCREEN_WIDTH * 0.85,SCREEN_HEIGHT * 0.8,arrowD,0.35)
+
+allSections = []
+sectionButtons = []
 
 def Start():
     running = True
-    numberOfSections = 6
+    numberOfSections = 7
+    index = 0
 
+    getTutorialSections()
+    getSectionRects(numberOfSections)
 
     while running:
         screen.fill((219, 200, 167))
 
         pygame.draw.rect(screen,(206,187,155),((SCREEN_WIDTH - INNER_SCREEN_WIDTH) // 2,0,INNER_SCREEN_WIDTH,SCREEN_HEIGHT))
-        addAllSections(numberOfSections)
 
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+            if event.type == pygame.QUIT:
+                running = False
 
         if exitButton.drawButton(screen):
             # exit page
             running = False
 
+        if arrowButtonUp.drawButton(screen):
+            if index - 1 >= 0:
+                index = index - 1
+
+        if arrowButtonDown.drawButton(screen):
+            if index + 7 < len(allSections):
+                index = index + 1
+
+        drawButtons(numberOfSections,index)
+
         pygame.display.update()
 
-def addAllSections(numberOfSections):
-    tutorialSectionHeight = 100
-    for section in range(numberOfSections):
-        startY = tutorialSectionHeight * section
-        pygame.draw.rect(screen,(245,245,245),((SCREEN_WIDTH - TUTORIAL_SECTION_WIDTH) // 2,startY * 1.2 + TUTORIAL_SECTION_PADDING,TUTORIAL_SECTION_WIDTH,tutorialSectionHeight),border_radius=10)
 
+def getSectionRects(sectionsToDisplay):
+    tutorialSectionHeight = 100
+    for i in range(sectionsToDisplay):
+        startY = tutorialSectionHeight * i
+        buttonRect = pygame.Rect((SCREEN_WIDTH - TUTORIAL_SECTION_WIDTH) // 2,startY * 1.2 + TUTORIAL_SECTION_PADDING,TUTORIAL_SECTION_WIDTH,tutorialSectionHeight)
+        sectionButtons.append(buttonRect)
+
+def drawButtons(sectionsToDisplay,index):
+    font = pygame.font.Font(None, 36)
+    for i in range(sectionsToDisplay):
+        button = sectionButtons[i]
+        newButton = pygame.draw.rect(screen,(245,245,245),button,border_radius=10)
+        surface = font.render(allSections[i+index][0], True, (0,0,0))
+        rect = surface.get_rect(center=button.center)
+        screen.blit(surface, rect)
 
 def getTutorialSections():
-    return []
+    tutorialData = open("tutorial\dialogue.txt","r")
+    for section in tutorialData.readlines():
+        if section.count(':') != 0:
+            newSection = section.split(':') # array containing [title of section, text to display, board for demo]
+            allSections.append([newSection[0],newSection[1],newSection[2]])
+
+    tutorialData.close()
+
 
 def getUserTutorialProgress():
     return []
